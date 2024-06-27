e1c90a1d8efcb12637b0a991b43f3334406ad9cc
Sandhya200307 <152253299+Sandhya200307@users.noreply.github.com>
2024-06-27 11:55:58 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChartComponent from './ChartComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Charting Library Assignment</h1>
+      <ChartComponent />
+    </div>
+  );
+};
+
+export default App;
+
